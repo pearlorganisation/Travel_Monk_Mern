38f160a41e36119e9c59f0953d713afb18d7d482
Saurabh Chamola <saurabh@pearlorganisation.com>
2024-09-20 12:02:30 +0530

    added fetch packages by id on package details page

--- src/features/store.jsx
+++ src/features/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import tripReducer from "../features/trips/tripsSlice";
+import packageReducer from "./package/packageSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     trip: tripReducer,
+    packages: packageReducer,
   },
 });
 export default store;
