de440e72be0b373fa621788790a1dd0fb7e2e0dd
Ranjan <ranjan@pearlorganisation.com>
2025-01-11 18:19:36 +0530

    added prime reactand made changes i about us page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <PrimeReactProvider>
         <App />
+        </PrimeReactProvider>
       </PersistGate>
     </Provider>
   </>
