377a72ec655b8ab5cfec47682741526668a66f21
Ranjan <ranjan@pearlorganisation.com>
2025-01-27 11:24:07 +0530

    minor changes

--- src/pages/Hotel/SingleHotelDetails.jsx
+++ src/pages/Hotel/SingleHotelDetails.jsx
@@ -145,7 +145,7 @@ const SingleHotelDetails = () => {
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 />
               </form>
-            ) : (
+             : (
               <></>
             )}
             <div className="text-xl font-semibold text-gray-800 mb-4">
