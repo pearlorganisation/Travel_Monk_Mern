f78fe5300dba902405a684a9a1550f17667b9ec9
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-02-04 17:30:00 +0530

    add data in accordians

--- src/components/AnimatedTabs/AnimatedTabs.jsx
+++ src/components/AnimatedTabs/AnimatedTabs.jsx
@@ -10,28 +10,44 @@ import {
 
 const tabs = [
   {
-    title: "How do UI components improve UX?",
-    id: "improve",
+    title: "Expertise & Experience ",
+    id: "1",
     description:
-      "UI components can improve UX by providing familiar, consistent interactions that make it easy for users to navigate and interact with an application.",
+      "With years of experience in the travel industry, our team brings a wealth of expertise to every aspect of your journey. We are passionate about crafting personalized, unforgettable travel experiences, backed by in-depth knowledge of destinations, local cultures, and the latest travel trends. Trust us to guide you with insider tips and insider access to make your travels smoother, smarter, and more enriching.",
     imageUrl:
-      "https://images.unsplash.com/photo-1709949908058-a08659bfa922?q=80&w=1200&auto=format",
+      "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=600",
   },
   {
-    title: "Important of UI component.",
-    id: "important",
+    title: "Comprehensive Travel Packages",
+    id: "2",
     description:
-      "Some common challenges include maintaining consistency across different devices and screen sizes, ensuring compatibility with various browsers and assistive technologies, and balancing flexibility with ease of use.",
+      "Our comprehensive travel packages are designed to cater to every type of traveler, from adventure seekers to those looking for relaxation. We offer all-inclusive itineraries that cover flights, accommodations, guided tours, and more—ensuring a hassle-free, well-rounded experience. Whether you're planning a quick getaway or an extended vacation, we’ve got a package that fits your needs and preferences perfectly.",
     imageUrl:
-      "https://images.unsplash.com/photo-1548192746-dd526f154ed9?q=80&w=1200&auto=format",
+      "https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    title: "Is UI and UX Same?",
-    id: "same",
+    title: "Safety & Reliability",
+    id: "3",
     description:
-      "     Developers can ensure the responsiveness of UI components by using techniques such as fluid layouts, flexible grids, and media queries to adapt the components to different screen sizes and orientations.",
+      "Your safety and peace of mind are our top priorities. We work with trusted partners and follow rigorous standards to ensure every aspect of your travel experience is reliable, secure, and seamless. Whether it’s booking accommodations, arranging transportation, or organizing tours, we make sure everything is handled with the utmost care and attention to detail, so you can focus on enjoying your journey with confidence.",
     imageUrl:
       "https://images.unsplash.com/photo-1693581176773-a5f2362209e6?q=80&w=1200&auto=format",
+  },
+  {
+    title: "Unique and Authentic Experiences",
+    id: "4",
+    description:
+      "We specialize in creating unique and authentic travel experiences that go beyond typical tourist attractions. From hidden gems to local cultural immersions, we connect you with real, meaningful encounters that allow you to truly experience a destination. Whether it’s dining with locals, exploring off-the-beaten-path adventures, or engaging in hands-on activities, we make sure your journey is one-of-a-kind.",
+    imageUrl:
+      "https://images.pexels.com/photos/691668/pexels-photo-691668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Exceptional Value",
+    id: "5",
+    description:
+      "We believe in offering exceptional value without compromising on quality. Our carefully curated travel packages are designed to give you the best experiences at competitive prices, ensuring you get the most out of every dollar spent. Whether it’s premium accommodations, exclusive activities, or seamless service, we work hard to provide you with unforgettable trips that deliver outstanding value.",
+    imageUrl:
+      "https://images.pexels.com/photos/590029/pexels-photo-590029.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
 function AnimatedImageTabs() {
