fcd790c5798829e919c2926703a0232ae9885883
Ranjan <ranjan@pearlorganisation.com>
2025-03-03 13:45:55 +0530

    ui fixes

--- src/pages/Home/supportingComponent/Distinguish.jsx
+++ src/pages/Home/supportingComponent/Distinguish.jsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { baseURL } from "../../../services/axiosInterceptor";
 import { Link } from "react-router-dom";
 
@@ -9,12 +9,13 @@ const Distinguish = ({ hotels }) => {
   return (
     <>
       <div className="bg-[#f5f5f5] py-8">
-        <div className="container mx-auto max-w-6xl">
+        <div className="container mx-auto max-w-6xl relative">
           <h2 className="text-center text-2xl font-bold mb-6">
             Distinguished Stays
           </h2>
 
           {/* Swiper Section */}
+          <div className="relative">
           <Swiper
             slidesPerView={1}
             spaceBetween={20}
@@ -35,7 +36,11 @@ const Distinguish = ({ hotels }) => {
                 spaceBetween: 10,
               },
             }}
-            modules={[Pagination]}
+            navigation={{
+              prevEl: ".swiper-button-prev",
+              nextEl: ".swiper-button-next",
+            }}
+            modules={[Pagination, Navigation]}
             className="mySwiper"
           >
             {Array.isArray(hotels) &&
@@ -76,16 +81,11 @@ const Distinguish = ({ hotels }) => {
                 </SwiperSlide>
               ))}
           </Swiper>
+          <div className="swiper-button-prev absolute lg:!-left-[50px] bg-[#007E8F] !text-[#ffff] !px-8 !py-8 !rounded-full !h-6 !w-6 !mt-4 top-1/2 transform -translate-y-1/2 z-10 !text-xs !font-semibold scale-50 flex items-center justify-center"></div>
 
-          {/* Explore All Link */}
-          <div className="text-right mt-4 mb-6">
-            <Link
-              to="/indian_packages"
-              className="text-blue-500 hover:underline"
-            >
-              Explore All →
-            </Link>
-          </div>
+          {/* Swiper Next Button */}
+          <button className="swiper-button-next absolute  lg:!-right-[50px] bg-[#007E8F] !text-[#ffff] !px-8 !py-8 !rounded-full !h-6 !w-6 !mt-4 top-1/2 transform -translate-y-1/2 z-10 !text-xs !font-semibold scale-50 flex items-center justify-center"></button>
+        </div>
         </div>
       </div>
     </>
