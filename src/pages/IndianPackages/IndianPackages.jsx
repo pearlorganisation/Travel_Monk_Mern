b5b53a4d01e02890f67aef9d1921016a364f02b7
Ranjan <ranjan@pearlorganisation.com>
2025-03-22 11:14:36 +0530

    minor fix

--- src/pages/IndianPackages/IndianPackages.jsx
+++ src/pages/IndianPackages/IndianPackages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DestinationCard from "../../components/DestinationCards/DestinationCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllDestinations } from "../../features/trips/tripActions";
